52d5275778eea24f935086ee706bcaaaf8f2e523
Arpit Aggarwal <arpitdec5@gmail.com>
2016-07-01 05:25:21 +0530

    second commit

---
+++ javajokelib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MyClass {
+
+    String arr[]={"Why do Java developers wear glasses? Because they can't C#",
+                    "In order to understand recursion you must first understand recursion."};
+
+    public String getJoke(){
+        return arr[0];
+    }
+}
